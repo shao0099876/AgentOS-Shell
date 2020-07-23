31a7d0e249a932e0a2db40267610d862159905a5
src <shao0099876@outlook.com>
2020-07-23 22:06:01 +0800

    7.23 晚
    增加opencv支持，完成图片读取实现

--- src/com/src/Main.java
+++ src/com/src/Main.java
@@ -1,6 +1,10 @@
 package com.src;
 
 import com.src.object.Scene;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -15,9 +19,10 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
     public static void main(String[] args) {
         if(args.length==0){
             System.exit(0);
@@ -36,9 +41,18 @@ public class Main {
         } catch (IOException | SAXException | ParserConfigurationException e) {
             e.printStackTrace();
         }
+        readScene(Global.script_root+"/scene/");
         //test
+
+    }
+    private static void readScene(String path){
         for(Scene i:Global.sceneArrayList){
-            System.out.println(i.id+" "+i.name);
+            Mat mat=Imgcodecs.imread(path+i.id+"/feature.bmp");
+            for(int x=0;x<mat.size().width;x++){
+                for(int y=0;y<mat.size().height;y++){
+                    System.out.println(x+" "+y+" "+ Arrays.toString(mat.get(x, y)));
+                }
+            }
         }
     }
     private static void readScript(String fileName) throws ParserConfigurationException, IOException, SAXException {
